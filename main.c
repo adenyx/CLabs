609baa998880beee69d37fb4d12622e3ff370e45
adenyx <59793388+adenyx@users.noreply.github.com>
2020-09-15 01:27:05 +0300

    v0.1 - made some notes

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include <string.h>
+
+//Ввести с клавиатуры строку символов.
+//Программа должна определить длину введенной строки,
+//и если длина четная, то удаляются 2 первых и 2 последних символа.
+int first() {
+    char userString[100];
+    int length;
+
+    printf("Input the line here: ");
+    scanf("%s", userString);
+    length = strlen(userString);
+    if (length % 2 > 0) {
+        userString[0] = (char) "\0";
+        userString[1] = (char) "\0";
+        userString[length-1] = (char) "\0";
+        userString[length] = (char) "\0";
+        for (int i = 0; i < length; i++) {
+            if ((char *) userString[i] != "\0") {
+                printf("%c", userString[i]);
+            }
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int choose;
+    printf("1 / 2: ");
+    scanf("%d\n", &choose);
+    /*if (choose == 1) {
+        first();
+    } else {
+        printf("Input Error!");
+    }*/
+    first();
     return 0;
 }
